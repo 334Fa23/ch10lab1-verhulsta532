0cfd34c4aafed960db95ef95373957017600ffaf
Allison Verhulst <122564024+verhulsta532@users.noreply.github.com>
2023-09-18 19:51:46 -0500

    Chapter 10 Lab
    Added hamburger menu on the mobile version of the website

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/* 
+Name: Allison Verhulst
+File name: script.js
+Date: 9/18/23
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
